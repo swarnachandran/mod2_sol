97532e95dda0dac283c2ff311fd2a6d12f696ebf
swarnachandran <74671975+swarnachandran@users.noreply.github.com>
2021-02-24 19:41:01 +0530

    Update speakhello.js

--- module4-solution/speakhello.js
+++ module4-solution/speakhello.js
@@ -1,8 +1,8 @@
 (function(window) {
-    var helloSpeaker = new Object();
+    var hellospeaker = new Object();
     var speakWord = "Hello";
-    helloSpeaker.speak = function(name) {
+    hellospeaker.speak = function(name) {
         console.log(speakWord + " " + name);
     };
-    window.helloSpeaker = helloSpeaker;
+    window.hellospeaker = hellospeaker;
 })(window);
