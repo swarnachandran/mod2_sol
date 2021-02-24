b273e7316b6c7a02e427b383d77ea7bb75542708
swarnachandran <74671975+swarnachandran@users.noreply.github.com>
2021-02-24 19:42:41 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,9 +3,9 @@
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
+            byespeaker.speak(names[i]);
         } else {
-            helloSpeaker.speak(names[i]);
+            hellospeaker.speak(names[i]);
         }
     }
 })();
