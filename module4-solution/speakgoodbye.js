f00161f5c3157519dce3fb73d661a82a22df4176
swarnachandran <74671975+swarnachandran@users.noreply.github.com>
2021-03-01 12:25:07 +0530

    Delete module4-solution directory

--- module4-solution/speakgoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    var speakWord = "Good Bye";
-    byespeaker.speak = function (name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byespeaker = byespeaker;
-})(window);
--- module4-solution/speakhello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var hellospeaker = new Object();
-    var speakWord = "Hello";
-    hellospeaker.speak = function(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.hellospeaker = hellospeaker;
-})(window);
